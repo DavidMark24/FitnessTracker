cd55e8d58648c9427fb31e3a0a51bf2d1b3a88e2
David Johnson <david.m.johnson32@gmail.com>
2021-03-21 11:47:58 -0700

    Added server api and html routes

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", {useNewUrlParser: true});
+
+// require('./routes/api-routes')(app)
+// require('./routes/html-routes')(app)
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}..`);
+});
